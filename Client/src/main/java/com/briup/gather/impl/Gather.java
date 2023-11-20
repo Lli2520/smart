9031d2e2f4e3708ce71a4ca8378c02c8ac44f0e1
lixin <lixin@bttc.edu.cn>
2023-11-20 15:32:49 +0800

    第一次提交

---
+++ Client/src/main/java/com/briup/gather/impl/Gather.java
@@ -0,0 +1,13 @@
+package com.briup.gather.impl;
+
+/**
+ * ClassName: Gatherimpl
+ * Package: com.briup.gather.impl
+ * Description:
+ *
+ * @Author lixin
+ * @Create 2023/11/20/020 15:04
+ * @Version 1.0
+ */
+public interface Gatherimpl {
+}
